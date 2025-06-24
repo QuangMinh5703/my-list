1bbad9e87e8f020d743d4b14d67aaa023db2a135
NguyenHuy5485 <huyc5485@gmail.com>
2025-06-25 01:41:40 +0700

    feat: back home main

--- app/src/main/java/com/example/mylist/MainActivity.java
+++ app/src/main/java/com/example/mylist/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
                 startActivity(intent);
-                finish();
+
             }
         });
     }
--- app/src/main/java/com/example/mylist/ProfileActivity.java
+++ app/src/main/java/com/example/mylist/ProfileActivity.java
@@ -1,21 +1,21 @@
 package com.example.mylist;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private Button btnLogout, btnChangePassword;
+    LinearLayout btnHome1;
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.profile);
-
         btnLogout = findViewById(R.id.btn_logout);
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,8 +35,28 @@ public class ProfileActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        initView();
+        listener();
+}
+    private void initView() {
+        View menu = findViewById(R.id.menu_profile);
+        btnHome1 = menu.findViewById(R.id.btn_home);
+    }
+
+    private void listener() {
+        btnHome1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+
+            }
+        });
+
     }
 }
+
+
 
 
 
